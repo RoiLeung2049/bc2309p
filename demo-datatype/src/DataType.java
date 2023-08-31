3072664997d6256c30b5d4afb27458baf5d4d52b
Roy.Leung <yawgmoth.rl@gmail.com>
2023-08-31 21:56:56 +0800

    testing update

---
+++ demo-datatype/src/DataType.java
@@ -0,0 +1,12 @@
+public class DataType {
+  public static void main(String[] args) {
+    String str = " Hello World!!!"; // Assign value  "Hello World!! " to a variable , str .  
+    System.out.println(str); //Print the value of  variable : "Hello World"
+    System.out.println("str"); // Print the String value : str
+    String box ; //Declaration 聲明  declare what datatype the variable box can save. 
+    box = "abc" ; //Assign value "abc" to a variable box . 
+    System.out.println(box); //abc
+
+
+  }
+}
---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+  // this is comment, I can type anything here . 
+  public static void main(String[] args) {
+    //Program starts ..... 
+    //Tools: System.out.println 
+    System.out.println("Hello World 123 123 ");
+    System.out.println("test test. ");
+  }
+}
